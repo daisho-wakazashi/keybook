d48851fcaf603ed06c2a8bd03c2b59615473f7b3
Andy <1270306+daisho-wakazashi@users.noreply.github.com>
2025-11-06 11:13:36 +1300

    create users incl seeds

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  enum :role, {
+    property_manager: "property_manager",
+    tenant: "tenant"
+  }, prefix: true
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :role, presence: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,29 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema[8.1].define(version: 2025_11_05_212042) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
+  # Custom types defined in this database.
+  # Note that some types may not work with other database engines. Be careful if changing database.
+  create_enum "user_role", ["property_manager", "tenant"]
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.enum "role", null: false, enum_type: "user_role"
+    t.datetime "updated_at", null: false
+    t.index ["role"], name: "index_users_on_role"
+  end
+end
