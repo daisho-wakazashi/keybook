885e302b6acefe33114c7f2f99590fe4b14af836
Andy <1270306+daisho-wakazashi@users.noreply.github.com>
2025-11-07 10:15:58 +1300

    set up testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,6 +16,7 @@ RSpec.configure do |config|
 
   config.use_transactional_fixtures = true
   config.include FactoryBot::Syntax::Methods
+  config.include ActiveSupport::Testing::TimeHelpers
 
   config.filter_rails_from_backtrace!
 end
